885928872714c7913570135c7d50297cee7ec717
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-03 21:30:10 +0100

    chore: fix imported ReentrancyGuard contract file path

--- contracts/src/BlindBoxGame.sol
+++ contracts/src/BlindBoxGame.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract BlindBoxGame {
     enum State {
