37ca733d635d51c6a15277dba88f29656cda85bd
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-03 20:25:13 +0100

    feat(contract:blindboxgame): add game state enum

--- contracts/src/BlindBoxGame.sol
+++ contracts/src/BlindBoxGame.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract BlindBoxGame {}
+contract BlindBoxGame {
+    enum State {
+        CREATED,
+        JOINED,
+        CHOOSING,
+        REVEALED,
+        FINISHED
+    }
+}
