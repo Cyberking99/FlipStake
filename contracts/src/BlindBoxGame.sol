9e5f88db752b39b358a6b6fa3a64da13565e3ebb
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-03 21:26:21 +0100

    feat: import ReentrancyGuard contract to BlindBoxGame

--- contracts/src/BlindBoxGame.sol
+++ contracts/src/BlindBoxGame.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 contract BlindBoxGame {
     enum State {
         CREATED,
