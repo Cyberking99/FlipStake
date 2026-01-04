02c8c8839b44a10461dad93d7d1ef81c6b72ce52
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-04 17:49:36 +0100

    feat: add wallet connect config

---
+++ frontend/config/index.tsx
@@ -0,0 +1,25 @@
+import { cookieStorage, createStorage, http } from "@wagmi/core";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { celoSepolia } from "@reown/appkit/networks";
+import { PROJECT_ID } from "@/lib/constants";
+
+// Get projectId from https://cloud.reown.com
+export const projectId = PROJECT_ID;
+
+if (!projectId) {
+  throw new Error("Project ID is not defined");
+}
+
+export const networks = [celoSepolia];
+
+//Set up the Wagmi Adapter (Config)
+export const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
+  projectId,
+  networks,
+});
+
+export const config = wagmiAdapter.wagmiConfig;
---
+++ frontend/context/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { wagmiAdapter, projectId } from "@/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAppKit } from "@reown/appkit/react";
+import { celoSepolia } from "@reown/appkit/networks";
+import React, { type ReactNode } from "react";
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+// import { PROJECT_NAME, PROJECT_DESCRIPTION, PROJECT_URL } from '@/lib/constants'
+const PROJECT_NAME = "FlipStake";
+const PROJECT_DESCRIPTION = "Stake, Flip, Win!";
+const PROJECT_URL = "https://flipstake.vercel.app";
+
+const queryClient = new QueryClient();
+
+if (!projectId) {
+  throw new Error("Project ID is not defined");
+}
+
+const metadata = {
+  name: PROJECT_NAME,
+  description: PROJECT_DESCRIPTION,
+  url: PROJECT_URL,
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
+
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks: [celoSepolia],
+  defaultNetwork: celoSepolia,
+  metadata: metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+function ContextProvider({
+  children,
+  cookies,
+}: {
+  children: ReactNode;
+  cookies: string | null;
+}) {
+  const initialState = cookieToInitialState(
+    wagmiAdapter.wagmiConfig as Config,
+    cookies,
+  );
+
+  return (
+    <WagmiProvider
+      config={wagmiAdapter.wagmiConfig as Config}
+      initialState={initialState}
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default ContextProvider;
