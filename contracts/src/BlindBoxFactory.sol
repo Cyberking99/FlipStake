02f6722d43408f40e229c47535c8db5825375a93
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-04 08:12:56 +0100

    feat: create function to create a new game

--- contracts/src/BlindBoxFactory.sol
+++ contracts/src/BlindBoxFactory.sol
@@ -5,4 +5,24 @@ import "./BlindBoxGame.sol";
 
 contract BlindBoxFactory {
     address[] public games;
+
+    function createGame(
+        bytes32 commitHash,
+        uint256 stake,
+        uint256 joinTimeout,
+        uint256 revealTimeout
+    ) external payable returns (address gameAddress) {
+        require(msg.value == stake, "Incorrect stake");
+
+        BlindBoxGame game = new BlindBoxGame{value: msg.value}(
+            msg.sender,
+            commitHash,
+            stake,
+            joinTimeout,
+            revealTimeout
+        );
+
+        gameAddress = address(game);
+        games.push(gameAddress);
+    }
 }
