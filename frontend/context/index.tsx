075c987ceaea71ac2c82a45b61c7e7626bf44d2b
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-05 23:20:46 +0100

    feat: switch to baseSepolia chain

--- frontend/context/index.tsx
+++ frontend/context/index.tsx
@@ -3,7 +3,7 @@
 import { wagmiAdapter, projectId } from "@/config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createAppKit } from "@reown/appkit/react";
-import { celoSepolia } from "@reown/appkit/networks";
+import { baseSepolia } from "@reown/appkit/networks";
 import React, { type ReactNode } from "react";
 import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
 // import { PROJECT_NAME, PROJECT_DESCRIPTION, PROJECT_URL } from '@/lib/constants'
@@ -27,8 +27,8 @@ const metadata = {
 const modal = createAppKit({
   adapters: [wagmiAdapter],
   projectId,
-  networks: [celoSepolia],
-  defaultNetwork: celoSepolia,
+  networks: [baseSepolia],
+  defaultNetwork: baseSepolia,
   metadata: metadata,
   features: {
     analytics: true, // Optional - defaults to your Cloud configuration
