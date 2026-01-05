a6bba94e54c336dc40e787b89dbed867cc2867f1
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-05 23:19:47 +0100

    feat: switch to baseSepolia chain

--- frontend/config/index.tsx
+++ frontend/config/index.tsx
@@ -1,6 +1,6 @@
 import { cookieStorage, createStorage, http } from "@wagmi/core";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { celoSepolia } from "@reown/appkit/networks";
+import { baseSepolia } from "@reown/appkit/networks";
 import { PROJECT_ID } from "@/lib/constants";
 
 // Get projectId from https://cloud.reown.com
@@ -12,7 +12,7 @@ if (!projectId) {
   throw new Error("Project ID is not defined");
 }
 
-export const networks = [celoSepolia];
+export const networks = [baseSepolia];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
