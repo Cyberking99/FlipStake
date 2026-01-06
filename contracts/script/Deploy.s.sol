fa5b0956ee925b712816db2dc1aeea13b34d54a7
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-06 08:21:58 +0100

    feat: create deployment script

---
+++ contracts/script/Deploy.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import {Script, Console} from "forge-std";
+import "../src/FlipStakeFactory.sol";
+
+contract DeployScript is Script {}
